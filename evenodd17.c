6a3bd2e323c4bea5887f4d4919a1c438256c630e
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 11:46:03 +0530

    Add files via upload

---
+++ evenodd17.c
@@ -0,0 +1,22 @@
+// C Program to Check Even or Odd Using Modulo Operator
+#include <stdio.h>
+
+void checkOddEven(int N) {
+  
+    int r = N % 2;
+
+    if (r == 0)  {
+        printf("Even");
+    }
+  
+
+    else  {
+        printf("Odd");
+    }
+}
+
+int main() {
+    int N = 101;
+    checkOddEven(N);
+    return 0;
+}

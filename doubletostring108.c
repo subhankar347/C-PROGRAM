81ae3dff3c6f91ecb7f4ecd29901ce8acc589c6c
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:32:42 +0530

    Add files via upload

---
+++ doubletostring108.c
@@ -0,0 +1,11 @@
+// C Program to demonstrate  Double to String Conversion 
+#include <stdio.h> 
+
+int main() 
+{ 
+	double n = 456321.7651234; 
+	char str[100]; 
+	sprintf(str, "%f", n); 
+	printf("the string is: %s\n", str); 
+	return 0; 
+}

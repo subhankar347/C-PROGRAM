ea0919efc290f3a25cc8bcf9a5f07dcfd6aa5e42
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:36:19 +0530

    Add files via upload

---
+++ chartoint112.c
@@ -0,0 +1,12 @@
+// C program to convert the given char to int using ASCII values
+#include <stdio.h>
+
+int main() {
+  
+    char ch = '7';
+
+    int N = ch - '0';
+    printf("The integer value of character '%c' is %d\n", ch, N);
+
+    return 0;
+}

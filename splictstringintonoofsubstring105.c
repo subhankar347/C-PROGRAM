b6822680d98e7f2f2e8ed9fc9b3459e5384f7e0a
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:27:27 +0530

    Add files via upload

---
+++ splictstringintonoofsubstring105.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void split(char *s, char *d) {
+  
+    char *ss = strtok(s, d);  
+    while (ss != NULL) {
+        printf("%s\n", ss);
+      
+        ss = strtok(NULL, d);  
+    }
+}
+int main() {
+    char s[] = "Geeks,for,Geeks"; 
+    char *d = ",";  
+
+    split(s, d);
+  
+    return 0;
+}

b0b0cae620b9b10719483b9c9ea5130ca298469d
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:37:33 +0530

    Add files via upload

---
+++ decimaltohexadecimal116.c
@@ -0,0 +1,11 @@
+// C Program to demonstrate Decimal to Hexadecimal Conversion using the format specifier 
+#include <stdio.h> 
+
+int main() 
+{ 
+	int decimalNumber = 45; 
+	
+
+	printf("Hexadecimal number is: %X", decimalNumber); 
+	return 0; 
+}

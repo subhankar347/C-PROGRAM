8853953ab7f46406c74e00677cd057560da4ee91
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:06:40 +0530

    Add files via upload

---
+++ powerrecution60.c
@@ -0,0 +1,30 @@
+// C program for the above approach 
+#include <stdio.h> 
+
+
+long power(int x, unsigned n) 
+{ 
+	
+	long long pow = 1; 
+
+	 
+	for (int i = 0; i < n; i++) { 
+		pow = pow * x; 
+	} 
+
+	return pow; 
+} 
+
+
+int main(void) 
+{ 
+
+	int x = 2; 
+	unsigned n = 3; 
+
+	 
+	int result = power(x, n); 
+	printf("%d", result); 
+
+	return 0; 
+}

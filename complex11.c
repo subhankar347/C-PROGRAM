7b54935018d15692de4f2656ece5ec08b715c1df
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 11:42:38 +0530

    Add files via upload

---
+++ complex11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct Complex {
+    float real;
+    float imag;
+};
+
+int main() {
+    struct Complex num1, num2, sum;
+
+    
+    printf("Enter real and imaginary parts of first complex number: ");
+    scanf("%f %f", &num1.real, &num1.imag);
+
+    
+    printf("Enter real and imaginary parts of second complex number: ");
+    scanf("%f %f", &num2.real, &num2.imag);
+
+    
+    sum.real = num1.real + num2.real;
+    sum.imag = num1.imag + num2.imag;
+
+    
+    printf("Sum of the complex numbers: %.2f + %.2fi\n", sum.real, sum.imag);
+
+    return 0;
+}

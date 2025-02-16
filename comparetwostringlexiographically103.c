88728e96cbc5f0917b6216d5e0f31ac6b4da8db1
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:26:39 +0530

    Add files via upload

---
+++ comparetwostringlexiographically103.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[] = "Geeks";
+    char s2[] = "gfg";
+
+    int res = strcmp(s1, s2);
+
+    if (res < 0)
+        printf("\"%s\" is smaller than \"%s\".\n", s1, s2);
+    else if (res > 0)
+        printf("\"%s\" is greater than \"%s\".\n", s1, s2);
+    else
+        printf("\"%s\" is equal to \"%s\".\n", s1, s2);
+
+    return 0;
+}

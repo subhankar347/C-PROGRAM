737241751087e87af6f2b8868755df8bc6dd14ef
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 11:53:49 +0530

    Add files via upload

---
+++ palindrome32.c
@@ -0,0 +1,39 @@
+// C Program to check if a number is a palindrome by reversing the number
+#include <stdio.h>
+
+int reverseNum(int N) {
+  
+      
+    int rev = 0;
+    while (N > 0) {
+
+        
+        int dig = N % 10;
+
+        
+        rev = rev * 10 + dig;
+
+    
+        N /= 10;
+    }
+    return rev;
+}
+
+int isPalindrome(int N) {
+  
+    
+    if (N < 0)
+        return 0;
+    return N == reverseNum(N);
+}
+
+int main() {
+    int N = 121;
+    if (isPalindrome(N)) {
+        printf("Yes\n");
+    }
+    else {
+        printf("No\n");
+    }
+    return 0;
+}

3d043a94d7c2101392c05a89cb787baa89ed47bf
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:51:55 +0530

    Add files via upload

---
+++ makeafilereadonly137.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  
+    
+    FILE *file = fopen("example.txt", "r");  
+
+    if (file == NULL) {
+        printf("Error!\n");
+        return 1;
+    }
+
+    printf("Successfull\n");
+
+    
+    fclose(file);  
+    return 0;
+}

ee62f6f1bd69e3a105a8309343f5c29534fc7d76
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:37:16 +0530

    Add files via upload

---
+++ hexadecimaltodecimal115.c
@@ -0,0 +1,14 @@
+// C program to demonstrate hexadecimal to decimal  conversion using format specifier 
+
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	int n; 
+
+	scanf("%x", &n); 
+	
+	printf("%d", n); 
+	return 0; 
+}

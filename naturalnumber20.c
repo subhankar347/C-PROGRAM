ab09b7078c24688147fa7b3b353e524509a4037e
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 11:47:34 +0530

    Add files via upload

---
+++ naturalnumber20.c
@@ -0,0 +1,20 @@
+// C Program to demonstrate Sum of Natural Numbers using while loops
+
+#include <stdio.h>
+int main()
+{
+	int i, s = 0;
+	int n = 10;
+	i = 1;
+
+	
+	while (i <= n) {
+	
+		
+		s += i;
+		i++;
+	}
+
+	printf("Sum = %d", s);
+	return 0;
+}

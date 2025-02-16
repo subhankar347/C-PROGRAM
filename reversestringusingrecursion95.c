2e431080e69a3c17c43910b157297e983b8e21a5
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:24:13 +0530

    Add files via upload

---
+++ reversestringusingrecursion95.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void revRecursive(char *l, char *r) {
+
+    if (l != r) {
+        char c = *l;
+        *l = *r;
+        *r = c;
+      
+        revRecursive(l + 1, r - 1);
+    }
+}
+
+void rev(char *s) {
+    revRecursive(s, s + strlen(s) - 1);
+}
+
+int main() {
+    char s[] = "GeeksforGeeks";
+
+    rev(s);
+
+    printf("%s", s);
+    return 0;
+}

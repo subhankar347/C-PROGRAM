e653b53cf6a402264937dcc713558c0366533db4
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 11:51:18 +0530

    Add files via upload

---
+++ lcm27.c
@@ -0,0 +1,22 @@
+// C program to find LCM of two numbers
+#include <stdio.h>
+
+
+int main()
+{
+    int x = 15, y = 25, max;
+    max = (x > y) ? x : y;
+
+    
+    while (1) {
+        if (max % x == 0 && max % y == 0) {
+            printf("The LCM of %d and %d is %d.", x, y,
+                   max);
+            break;
+        }
+
+        ++max;
+    }
+
+    return 0;
+}

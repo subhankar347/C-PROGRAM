bcbb215e9c1a441a4fe2d180f8c1b103ccec7fab
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 11:46:44 +0530

    Add files via upload

---
+++ vowel18.c
@@ -0,0 +1,21 @@
+// C program to check if a character is a vowel or consonant
+#include <stdio.h>
+
+
+int main()
+{
+	char ch = 'A';
+
+
+	if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E'
+		|| ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O'
+		|| ch == 'u' || ch == 'U') {
+
+		printf("The character %c is a vowel.\n", ch);
+	}
+	else {
+		printf("The character %c is a consonant.\n", ch);
+	}
+
+	return 0;
+}

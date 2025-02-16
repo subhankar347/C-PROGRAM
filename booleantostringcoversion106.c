097506a811859ef6c3e8a96b67f36b899724f5d6
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 12:31:59 +0530

    Add files via upload

---
+++ booleantostringcoversion106.c
@@ -0,0 +1,15 @@
+// C program to demonstrate Boolean to String Conversion using conditional statements
+#include <stdbool.h>
+#include <stdio.h>
+int main()
+{
+
+	bool n = true;
+	if (n == true) {
+		printf("true");
+	}
+	else {
+		printf("false");
+	}
+	return 0;
+}

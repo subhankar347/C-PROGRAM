0a5b2a63a7457309576782252b9eb99e19a96cd9
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 11:42:14 +0530

    Add files via upload

---
+++ size10.c
@@ -0,0 +1,19 @@
+// C Program to Find the Size of int, float, double, and
+// char using sizeof operator directly
+#include <stdio.h>
+
+int main() {
+  
+    printf("Size of int: %u bytes\n", sizeof(int));
+
+    
+    printf("Size of float: %u bytes\n", sizeof(float));
+
+    
+    printf("Size of double: %u bytes\n", sizeof(double));
+
+    
+    printf("Size of char: %u bytes\n", sizeof(char));
+
+    return 0;
+}

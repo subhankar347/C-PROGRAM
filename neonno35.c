b9bb3d8421afc463f8a110d1a45946b54d1054b9
subhankar347 <palsubhankar116@gmail.com>
2025-02-16 11:55:37 +0530

    Add files via upload

---
+++ neonno35.c
@@ -0,0 +1,44 @@
+// C program to demonstrate whether  a number is Neon number or not 
+#include <stdio.h> 
+
+int Check_Neon_Number(int num) { 
+	int square = num * num; 
+
+	int n = square;
+	int digit; 
+
+
+	int sum = 0; 
+
+
+	while (n != 0) { 
+
+		
+		digit = n % 10; 
+		sum = sum + digit; 
+		n = n / 10; 
+	} 
+
+	
+	if (sum == num) 
+		return 1; 
+	else
+		return 0; 
+} 
+
+int main() 
+{ 
+	int num = 9; 
+
+	
+	int ans = Check_Neon_Number(num); 
+	if (ans == 1) 
+
+		
+		printf("true"); 
+	else
+
+		
+		printf("false"); 
+	return 0; 
+}
